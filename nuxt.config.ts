f4eeb0e80670e6dc5b385c00262daef79e381b8d
HenriqueKlaveren <henrique.klaveren@brandshub.com.br>
2024-01-22 16:04:06 -0300

    perf(nuxt-config): optimize configs

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss', '@nuxt/test-utils/module', '@pinia/nuxt'],
+  app: {
+    pageTransition: { name: 'slide-right', mode: 'out-in' },
+  },
+  modules: [
+    '@nuxtjs/tailwindcss',
+    '@nuxt/test-utils/module', // 'nuxt-lazy-load',
+    '@pinia/nuxt',
+    '@nuxt/image',
+  ],
   pinia: {
     storesDirs: ['./stores/**'],
   },
@@ -9,5 +17,22 @@ export default defineNuxtConfig({
     exposeConfig: true,
     viewer: true,
     // and more...
+  },
+  css: ['@/assets/scss/main.scss'],
+  imports: {
+    dirs: [
+      // Scan top-level modules
+      'composables/**',
+      'middleware/**',
+      'stores/**',
+      // "utils/**",
+    ],
+  },
+  runtimeConfig: {
+    public: {
+      TMDP_API_KEY: process.env.TMDP_API_KEY,
+      TMDB_ACCESS_TOKEN: process.env.TMDB_ACCESS_TOKEN,
+      TMDB_BASE_URL: process.env.TMDB_BASE_URL,
+    },
   },
 })
