46cc8002b9c642359ef2a388a9a01a43bc293e50
klaveren <klaveren@gmail.com>
2024-01-23 11:16:01 -0300

    perf(footer): update copy text

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,7 +1,7 @@
 <template>
   <footer class="fixed bottom-0 w-full bg-gray-800 p-4 z-50">
     <div class="container mx-auto text-white text-center">
-      &copy; 2024 Meu Projeto. Todos os direitos reservados.
+      &copy; 2024 Moovee. Todos os direitos reservados.
     </div>
   </footer>
 </template>
