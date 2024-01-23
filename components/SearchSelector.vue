291d498d8e474ed8e97efdfb7a3840cb4faedd63
klaveren <klaveren@gmail.com>
2024-01-23 11:17:33 -0300

    perf(SearchSelector): spli to external file staticGenres

--- components/SearchSelector.vue
+++ components/SearchSelector.vue
@@ -75,6 +75,8 @@
 <script setup>
 import { reactive } from 'vue'
 import { useMovieStore } from '~/stores/movie'
+import { staticGenres } from '~/core/staticGenres'
+
 const movieStore = useMovieStore()
 const formData = reactive({
   // searchAllReleases: false,
@@ -97,84 +99,7 @@ const submitFilters = async () => {
   await movieStore.search('/discover/movie', searchParams)
 }
 
-const genres = [
-  {
-    id: 28,
-    name: 'Action',
-  },
-  {
-    id: 12,
-    name: 'Adventure',
-  },
-  {
-    id: 16,
-    name: 'Animation',
-  },
-  {
-    id: 35,
-    name: 'Comedy',
-  },
-  {
-    id: 80,
-    name: 'Crime',
-  },
-  {
-    id: 99,
-    name: 'Documentary',
-  },
-  {
-    id: 18,
-    name: 'Drama',
-  },
-  {
-    id: 10751,
-    name: 'Family',
-  },
-  {
-    id: 14,
-    name: 'Fantasy',
-  },
-  {
-    id: 36,
-    name: 'History',
-  },
-  {
-    id: 27,
-    name: 'Horror',
-  },
-  {
-    id: 10402,
-    name: 'Music',
-  },
-  {
-    id: 9648,
-    name: 'Mystery',
-  },
-  {
-    id: 10749,
-    name: 'Romance',
-  },
-  {
-    id: 878,
-    name: 'Science Fiction',
-  },
-  {
-    id: 10770,
-    name: 'TV Movie',
-  },
-  {
-    id: 53,
-    name: 'Thriller',
-  },
-  {
-    id: 10752,
-    name: 'War',
-  },
-  {
-    id: 37,
-    name: 'Western',
-  },
-]
+const genres = staticGenres
 </script>
 
 <style scoped></style>
